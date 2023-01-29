ee050dc13fd889fd7bc32393c8bdd3d79ff8d725
Jung-GunSong <jsong781@gmail.com>
2023-01-28 19:40:45 -0500

    name tag with name change via input and button

--- app.js
+++ app.js
@@ -4,3 +4,13 @@
 
 // eslint-disable-next-line no-console
 console.log('Hello World');
+const nameEl = document.getElementById('name');
+
+nameEl.textContent = 'Jung';
+
+const updateButton = document.getElementById('update');
+updateButton.addEventListener('click', () => {
+    console.log('I am clicking the button');
+    const nameInput = document.getElementById("input");
+    nameEl.textContent = nameInput.value;
+});
